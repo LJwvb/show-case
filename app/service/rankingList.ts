cb6138c4d4f5d922963db293bc4d68f203da9828
LJwvb <z751368560@126.com>
2023-02-17 23:04:53 +0800

    fix：排行榜 排序

--- app/service/rankingList.ts
+++ app/service/rankingList.ts
@@ -5,6 +5,8 @@ export default class RankingList extends Service {
     const { app } = this;
     try {
       const result = await app.mysql.select('ranking_list');
+      // 将排行榜按照点赞数量高低排序
+      result.sort((a: any, b: any) => b.get_likes_num - a.get_likes_num);
       return result;
     } catch (err) {
       return null;
